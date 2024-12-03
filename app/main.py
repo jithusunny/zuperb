842c78b93a3752df41c103f1ac04f736b64f3c04
Jithu Sunny <jithusunnyk@gmail.com>
2024-12-04 00:11:45 +0530

    create table if not exists

--- app/main.py
+++ app/main.py
@@ -19,6 +19,9 @@ DATABASE_URL = "sqlite:///./zuperb.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Create tables if they don't exist
+Base.metadata.create_all(bind=engine)
+
 # Dependency to get the database session
 def get_db():
     db = SessionLocal()
